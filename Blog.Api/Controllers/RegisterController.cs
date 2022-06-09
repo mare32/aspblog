b709ef9f8217fb83a062c7176347f52bf7e1d3bd
mare32 <marechl32@gmail.com>
2022-06-09 19:35:23 +0200

    Register Swagger

--- Blog.Api/Controllers/RegisterController.cs
+++ Blog.Api/Controllers/RegisterController.cs
@@ -16,8 +16,33 @@ namespace Blog.Api.Controllers
         {
             _handler = handler;
         }
+        /// <summary>
+        /// Registers a user.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="command"></param>
+        /// <returns>HttpResponseMessage</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /api/register
+        ///     {
+        ///         "firstname": "Pera",
+        ///         "lastname": "Peric",
+        ///         "username": "perica",
+        ///         "email" : "perica@gmail.com",
+        ///         "password" : "321sifra"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Created.</response>
+        /// <response code="422">Validation failure.</response>
+        /// <response code="500">Unexpected server error.</response>
         [HttpPost]
         [AllowAnonymous]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(422)]
+        [ProducesResponseType(500)]
         public IActionResult Post([FromBody] RegisterDto request,[FromServices]IRegisterUserCommand command)
         {
             _handler.HandleCommand(command, request);
