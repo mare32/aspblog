eaf76657415b22dc1a0e2dc8b3678f88ccc5afd4
mare32 <marechl32@gmail.com>
2022-06-09 18:14:30 +0200

    UserUseCases Swagger

--- Blog.Api/Controllers/UserUseCasesController.cs
+++ Blog.Api/Controllers/UserUseCasesController.cs
@@ -19,7 +19,29 @@ namespace Blog.Api.Controllers
             _handler = handler;
         }
 
+
+        /// <summary>
+        /// Update users use cases
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///  PUT /api/userUseCases
+        ///  {
+        ///  "userId": "2",
+        ///  "useCaseIds": [2,2017,2007,2012]
+        ///  }
+        /// </remarks>
+        /// <response code="204">No content.</response>
+        /// <response code="401">Unauthorized.</response>
+        /// <response code="500">Unexpected server error.</response>
         [HttpPut]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(500)]
         public IActionResult Put([FromBody] UpdateUserUseCasesDto dto, [FromServices]IUpdateUserUseCasesCommand command)
         {
             _handler.HandleCommand(command, dto);
