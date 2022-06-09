299530dd233c1d872989b580aaaccb172c4bcada
mare32 <marechl32@gmail.com>
2022-06-09 18:19:55 +0200

    UseCaseLogs Swagger

--- Blog.Api/Controllers/UseCaseLogsController.cs
+++ Blog.Api/Controllers/UseCaseLogsController.cs
@@ -18,6 +18,26 @@ namespace Blog.Api.Controllers
         {
             _handler = handler;
         }
+        /// <summary>
+        /// Search use case logs
+        /// </summary>
+        /// <param name="search"></param>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///  GET /api/useCaseLogs
+        /// QueryString
+        ///  "username": "pera",
+        ///  "useCaseName": "create",
+        ///  "dateFrom": "2022-06-06",
+        ///  "dateTo": "2022-06-10",
+        ///  "perPage": 10,
+        ///  "page" : 2
+        /// </remarks>
+        /// <response code="401">Unauthorized.</response>
+        /// <response code="500">Unexpected server error.</response>
         [HttpGet]
         public IActionResult Get([FromQuery] UseCaseLogSearch search, [FromServices] IGetUseCaseLogsQuery query)
         {
